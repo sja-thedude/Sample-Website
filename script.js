3672bcfefbc850d97d575aa6ea12fb0ce4aeedd7
Syeda Juveria Afreen <sja.affu765@gmail.com>
2024-12-08 17:08:13 +0300

    Update

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburgerMenu').addEventListener('click', function() {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active');
+});
